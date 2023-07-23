ad8bb5c40f52d6d0b48e15f353c9a6447eee983c
Kirill Bordiyan <kirillbordiyan@gmail.com>
2023-07-23 16:20:07 +0300

    lec 2 files

---
+++ src/main/java/kirill/java_lecture/lec2/ProgramLec2.java
@@ -0,0 +1,33 @@
+package kirill.java_lecture.lec2;
+import java.io.File;
+
+public class ProgramLec2 {
+    //API - application programming interface - контракт, который предоставляет программа
+    //ко мне можно обратиться и я сделаю
+
+
+//    Создать строку из 1 млн плюсиков. Как?
+//    String str = "";
+//      for (int i = 0; i < 1_000_000; i++) {
+//        str += "+";
+//    }
+// ≈41000 ms
+
+//    StringBuilder sb = new StringBuilder();
+//      for (int i = 0; i < 1_000_000; i++) {
+//        sb.append("+");
+//    }
+// ≈9 ms
+    //добавлять - StringBuilder
+    //разбирать полученное - String
+
+
+    //файлы
+//    Для работы нужно:
+//    File <имя> = new File(<полный путь к файлу>);
+
+//    File f1 = new File("file.txt");
+//    File f2 = new File("/Users/sk/vscode/java_projects/file.txt");
+
+
+}
