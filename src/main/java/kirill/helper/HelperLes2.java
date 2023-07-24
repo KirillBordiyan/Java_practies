0dcd29219561f464b428724e90fb9427929d4f9f
Kirill Bordiyan <kirillbordiyan@gmail.com>
2023-07-24 19:10:56 +0300

    les 2 init

---
+++ src/main/java/kirill/helper/HelperLes2.java
@@ -0,0 +1,7 @@
+package kirill.helper;
+import java.util.Collections;
+import java.util.List;
+
+public class HelperLes2 {
+
+}
---
+++ src/main/java/kirill/program/ProgramLes2.java
@@ -0,0 +1,7 @@
+package kirill.program;
+import kirill.helper.HelperLes2;
+import java.util.List;
+
+public class ProgramLes2 {
+
+}
