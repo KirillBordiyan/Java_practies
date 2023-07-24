6dceda4cecae41e65878ab6b8d4e5b19876b8540
Kirill Bordiyan <kirillbordiyan@gmail.com>
2023-07-24 21:22:37 +0300

    sem 2 java

--- src/main/java/kirill/program/ProgramLes2.java
+++ src/main/java/kirill/program/ProgramLes2.java
@@ -1,7 +1,26 @@
 package kirill.program;
-import kirill.helper.HelperLes2;
-import java.util.List;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 
 public class ProgramLes2 {
+     public static void main(String[] args) {
+         Path path = Path.of("input.txt"); //описывает путь
+         boolean exist = Files.exists(path);
+         System.out.println(exist); //true, существует ли файл
 
+         //создание своего
+         //dir/file.txt
+
+//         Path dir = Path.of("root","dir");
+//         Files.createDirectory(dir);
+//
+//         Path file = Path.of("dir", "file.txt");
+//         Files.createDirectory(file);
+//
+//         Files.writeString(path,"cont cont", StandardOpenOption.WRITE); //запись, параметр StandardOpenOption можно менять
+
+    }
 }
